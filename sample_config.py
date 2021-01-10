b595d6416643db180dfc2457987bf8d4d5ba4437
profiregod <75692717+profiregod@users.noreply.github.com>
2021-01-10 22:15:22 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,4 +1,10 @@
 jio_saavn_api = "http://127.0.0.1:5000/result/?query="
+
 radio_link = "http://peridot.streamguys.com:7150/Mirchi"
-sudo_chat_id = -1001485393652
-owner_id = 1243703097
+
+sudo_chat_id = -1001443918903
+
+owner_id = 1016547547
+
+
+
